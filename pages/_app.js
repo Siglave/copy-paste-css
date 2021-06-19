1b66ba93521f1c435f0057b2f629e46ee85dd3a3
Pierre <pierrerouzaud18@gmail.com>
2021-06-19 20:55:58 +0200

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { theme } from '../frontend/constant';
+
+
+const GlobalStyle = createGlobalStyle`
+* {
+  outline: none;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: ${theme.fontFamilyText};
 }
+body {
+  font-family: sans-serif;
+  color: ${theme.grey8} ;
+  font-family: ${theme.fontFamilyText};
+  font-size: 16px;
+  
+  min-height: 100vh;
+}
+h1,h2,h3,h4,h5,h6{
+  font-family: ${theme.fontFamilyTitle};
+  font-weight: 400;
+}
+a{
+  background-color: transparent;
+  text-decoration: none;
+  outline: none;
+  cursor: pointer;
+  color: inherit ;
+}
+`
 
-export default MyApp
+
+
+export default function App({ Component, pageProps }) {
+
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
