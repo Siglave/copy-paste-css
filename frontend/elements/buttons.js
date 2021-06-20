5f0a169eab1bdc7798c74e14debff53896cb1ad0
Pierre <pierrerouzaud18@gmail.com>
2021-06-20 16:40:38 +0200

    Wip

--- frontend/elements/buttons.js
+++ frontend/elements/buttons.js
@@ -17,6 +17,7 @@ export const BUTTONS = [
             {
                 css: `
                     cursor: pointer;
+                    outline: 0;
                     color: #fff;
                     background-color: #0d6efd;
                     border-color: #0d6efd;
@@ -39,6 +40,7 @@ export const BUTTONS = [
             {
                 css: `
                     cursor: pointer;
+                    outline: 0;
                     display: inline-block;
                     font-weight: 400;
                     line-height: 1.5;
@@ -103,6 +105,7 @@ export const BUTTONS = [
             {
                 css: `
                     display: inline-block;
+                    outline: 0;
                     border: 0;
                     cursor: pointer;
                     background-color: #4299e1;
@@ -117,6 +120,7 @@ export const BUTTONS = [
             {
                 css: `
                     display: inline-block;
+                    outline: 0;
                     border: 0;
                     cursor: pointer;
                     background-color: #4299e1;
@@ -132,6 +136,7 @@ export const BUTTONS = [
             {
                 css: `
                     display: inline-block;
+                    outline: 0;
                     border: 0;
                     cursor: pointer;
                     background-color: #4299e1;
@@ -146,6 +151,7 @@ export const BUTTONS = [
             {
                 css: `
                     display: inline-block;
+                    outline: 0;
                     border: 0;
                     cursor: pointer;
                     background-color: white;
@@ -161,6 +167,7 @@ export const BUTTONS = [
             {
                 css: `
                     display: inline-block;
+                    outline: 0;
                     border: 0;
                     cursor: pointer;
                     background-color: white;
@@ -182,6 +189,7 @@ export const BUTTONS = [
             {
                 css: `
                     display: inline-block;
+                    outline: 0;
                     border: none;
                     cursor: pointer;
                     font-weight: 600;
@@ -196,6 +204,7 @@ export const BUTTONS = [
             {
                 css: `
                     display: inline-block;
+                    outline: 0;
                     border: none;
                     cursor: pointer;
                     font-weight: 600;
@@ -207,6 +216,86 @@ export const BUTTONS = [
                     padding: 0 10px;
                 `
             },
+        ]
+    },
+    {
+        company: "GitHub",
+        designSystemUrl: "https://primer.style/css/utilities/animations",
+        elements: [
+            {
+                css: `
+                    display: inline-block;
+                    cursor: pointer;
+                    padding: 5px 16px;
+                    font-size: 14px;
+                    font-weight: 500;
+                    line-height: 20px;
+                    vertical-align: middle;
+                    border: 1px solid;
+                    border-radius: 6px;
+                    color: #ffffff;
+                    background-color: #2ea44f;
+                    border-color: #1b1f2326;
+                    box-shadow: rgba(27, 31, 35, 0.04) 0px 1px 0px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset;
+                    transition: 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+                    transition-property: color, background-color, border-color;
+                    :hover {
+                        background-color: #2c974b;
+                        border-color: #1b1f2326;
+                        transition-duration: 0.1s;
+                    }
+                `
+            },
+            {
+                css: `
+                    display: inline-block;
+                    cursor: pointer;
+                    padding: 5px 16px;
+                    font-size: 14px;
+                    font-weight: 500;
+                    line-height: 20px;
+                    vertical-align: middle;
+                    border: 1px solid;
+                    border-radius: 6px;
+                    color: #24292e;
+                    background-color: #fafbfc;
+                    border-color: #1b1f2326;
+                    box-shadow: rgba(27, 31, 35, 0.04) 0px 1px 0px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset;
+                    transition: 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+                    transition-property: color, background-color, border-color;
+                    :hover {
+                        background-color: #f3f4f6;
+                        border-color: #1b1f2326;
+                        transition-duration: 0.1s;
+                    }
+                `
+            },
+            {
+                css: `
+                    display: inline-block;
+                    cursor: pointer;
+                    padding: 5px 16px;
+                    font-size: 14px;
+                    font-weight: 500;
+                    line-height: 20px;
+                    vertical-align: middle;
+                    border: 1px solid;
+                    border-radius: 6px;
+                    color: #0366d6;
+                    background-color: #fafbfc;
+                    border-color: #1b1f2326;
+                    box-shadow: rgba(27, 31, 35, 0.04) 0px 1px 0px 0px, rgba(255, 255, 255, 0.25) 0px 1px 0px 0px inset;
+                    transition: 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+                    transition-property: color, background-color, border-color;
+                    :hover {
+                        color: #ffffff;
+                        background-color: #0366d6;
+                        border-color: #1b1f2326;
+                        box-shadow: rgba(27, 31, 35, 0.1) 0px 1px 0px 0px, rgba(255, 255, 255, 0.03) 0px 1px 0px 0px inset;
+                        transition-duration: 0.1s;
+                    }
+                `
+            }
         ]
     }
 ]
