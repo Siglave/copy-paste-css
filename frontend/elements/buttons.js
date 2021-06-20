f609e143c83643a483291840c09e0531cd54998a
Pierre <pierrerouzaud18@gmail.com>
2021-06-20 23:28:34 +0200

    Wip

--- frontend/elements/buttons.js
+++ frontend/elements/buttons.js
@@ -79,6 +79,10 @@ export const BUTTONS = [
                     padding: 4px 8px;
                     display: inline-block;
                     min-height: 28px;
+                    transition: background-color .24s,box-shadow .24s;
+                    :hover {
+                        box-shadow: rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 12%) 0px 1px 1px 0px, rgb(84 105 212) 0px 0px 0px 1px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(60 66 87 / 8%) 0px 3px 9px 0px, rgb(60 66 87 / 8%) 0px 2px 5px 0px;
+                    }
                 `
             },
             {
@@ -95,6 +99,10 @@ export const BUTTONS = [
                     padding: 4px 8px;
                     display: inline-block;
                     min-height: 28px;
+                    transition: background-color .24s,box-shadow .24s;
+                    :hover {
+                        box-shadow: rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 12%) 0px 1px 1px 0px, rgb(60 66 87 / 16%) 0px 0px 0px 1px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(60 66 87 / 8%) 0px 3px 9px 0px, rgb(60 66 87 / 8%) 0px 2px 5px 0px;
+                    }
                 `
             }
         ],
