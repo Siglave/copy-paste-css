fe75e0d33dd3a6bb1932254a183b394711148806
Pierre <pierrerouzaud18@gmail.com>
2021-06-21 21:15:38 +0200

    wip

--- frontend/Layout.jsx
+++ frontend/Layout.jsx
@@ -23,7 +23,7 @@ const Layout = ({ children }) => {
                 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Allan:wght@700&display=swap" rel="stylesheet" />
                 <link rel="icon" href="/favicon.ico" />
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-78CQD51DV7"></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-MV2KWMVKTK"></script>
                 <script dangerouslySetInnerHTML={{
                     __html: `
                     window.dataLayer = window.dataLayer || [];
