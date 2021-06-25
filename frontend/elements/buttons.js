a26faded2e8ff177c23df871a04be720bf09f143
Pierre <pierrerouzaud18@gmail.com>
2021-06-26 00:39:51 +0200

    new btn

--- frontend/elements/buttons.js
+++ frontend/elements/buttons.js
@@ -1614,4 +1614,49 @@ export const BUTTONS = [
             },
         ]
     },
+    {
+        company: "RemoteOK",
+        elements: [
+            {
+                css: `
+                display: inline-block;
+                outline: 0;
+                cursor: pointer;
+                border-radius: 6px;
+                border: 2px solid #ff4742;
+                color: #fff;
+                background-color: #ff4742;
+                padding: 8px;
+                box-shadow: rgba(0, 0, 0, 0.07) 0px 2px 4px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1.5px 0px;
+                font-weight: 800;
+                font-size: 16px;
+                height: 42px;
+                :hover{
+                    background: 0 0;
+                    color: #ff4742;
+                }
+                `
+            },
+            {
+                css: `
+                display: inline-block;
+                outline: 0;
+                cursor: pointer;
+                border-radius: 6px;
+                border: 2px solid #ff4742;
+                color: #ff4742;
+                background: 0 0;
+                padding: 8px;
+                box-shadow: rgba(0, 0, 0, 0.07) 0px 2px 4px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1.5px 0px;
+                font-weight: 800;
+                font-size: 16px;
+                height: 42px;
+                :hover{
+                    background-color: #ff4742;
+                    color: #fff;
+                }
+                `
+            },
+        ]
+    },
 ]
