2edb0bd7fec541536ac4a4c3b28ed422de3f214a
Pierre <pierrerouzaud18@gmail.com>
2021-06-27 02:19:25 +0200

    Add next.js buttons

--- frontend/elements/buttons.js
+++ frontend/elements/buttons.js
@@ -1709,4 +1709,53 @@ export const BUTTONS = [
             },
         ]
     },
+    {
+        company: "Next.js",
+        elements: [
+            {
+                css: `
+                display: inline-block;
+                outline: 0;
+                cursor: pointer;
+                border: none;
+                padding: 0 56px;
+                height: 45px;
+                line-height: 45px;
+                border-radius: 7px;
+                background-color: #0070f3;
+                color: white;
+                font-weight: 400;
+                font-size: 16px;
+                box-shadow: 0 4px 14px 0 rgb(0 118 255 / 39%);
+                transition: background 0.2s ease,color 0.2s ease,box-shadow 0.2s ease;
+                :hover{
+                    background: rgba(0,118,255,0.9);
+                    box-shadow: 0 6px 20px rgb(0 118 255 / 23%);
+                }
+                `
+            },
+            {
+                css: `
+                display: inline-block;
+                outline: 0;
+                cursor: pointer;
+                border: none;
+                padding: 0 56px;
+                height: 45px;
+                line-height: 45px;
+                border-radius: 7px;
+                font-weight: 400;
+                font-size: 16px;
+                background: #fff;
+                color: #696969;
+                box-shadow: 0 4px 14px 0 rgb(0 0 0 / 10%);
+                transition: background 0.2s ease,color 0.2s ease,box-shadow 0.2s ease;
+                :hover{
+                    background: rgba(255,255,255,0.9);
+                    box-shadow: 0 6px 20px rgb(93 93 93 / 23%);
+                }
+                `
+            },
+        ]
+    },
 ]
