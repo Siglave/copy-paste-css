d2a2c350ec7476a21de1345a28e1c1792bf58892
Pierre <pierrerouzaud18@gmail.com>
2021-06-29 02:53:13 +0200

    Add figma btn

--- frontend/elements/buttons.js
+++ frontend/elements/buttons.js
@@ -1837,4 +1837,49 @@ export const BUTTONS = [
 
         ]
     },
+    {
+        company: "Figma",
+        elements: [
+            {
+                css: `
+                display: inline-block;
+                outline: 0;
+                border: 0;
+                cursor: pointer;
+                background: #000000;
+                color: #FFFFFF;
+                border-radius: 8px;
+                padding: 14px 24px 16px;
+                font-size: 18px;
+                font-weight: 700;
+                line-height: 1;
+                transition: transform 200ms,background 200ms;
+                :hover{
+                    transform: translateY(-2px);
+                }
+                `
+            },
+            {
+                css: `
+                display: inline-block;
+                outline: 0;
+                border: 0;
+                cursor: pointer;
+                border-radius: 8px;
+                padding: 14px 24px 16px;
+                font-size: 18px;
+                font-weight: 700;
+                line-height: 1;
+                transition: transform 200ms,background 200ms;
+                background: transparent;
+                color: #000000;
+                box-shadow: 0 0 0 3px #000000 inset;
+                :hover{
+                    transform: translateY(-2px);
+                }
+                `
+            },
+
+        ]
+    },
 ]
