66de8036a6e459741168d9d9577d461423a221bb
Pierre <pierrerouzaud18@gmail.com>
2021-12-03 00:14:46 +0100

    Add gumroad btn

--- frontend/elements/buttons.js
+++ frontend/elements/buttons.js
@@ -2033,5 +2033,71 @@ export const BUTTONS = [
                 `
             },
         ]
+    },
+    {
+        company: "Gumroad",
+        elements: [
+            {
+                css: `
+                    outline: 0;
+                    grid-gap: 8px;
+                    align-items: center;
+                    background-color: #ff90e8;
+                    color: #000;
+                    border: 1px solid #000;
+                    border-radius: 4px;
+                    cursor: pointer;
+                    display: inline-flex;
+                    flex-shrink: 0;
+                    font-size: 16px;
+                    gap: 8px;
+                    justify-content: center;
+                    line-height: 1.5;
+                    overflow: hidden;
+                    padding: 12px 16px;
+                    text-decoration: none;
+                    text-overflow: ellipsis;
+                    transition: all .14s ease-out;
+                    white-space: nowrap;
+                    :hover {
+                        box-shadow: 4px 4px 0 #000;
+                        transform: translate(-4px,-4px);
+                    }
+                    :focus-visible{
+                        outline-offset: 1px;
+                    }
+                `
+            },
+            {
+                css: `
+                    outline: 0;
+                    grid-gap: 8px;
+                    align-items: center;
+                    background: 0 0;
+                    border: 1px solid #000;
+                    border-radius: 4px;
+                    cursor: pointer;
+                    display: inline-flex;
+                    flex-shrink: 0;
+                    font-size: 16px;
+                    gap: 8px;
+                    justify-content: center;
+                    line-height: 1.5;
+                    overflow: hidden;
+                    padding: 12px 16px;
+                    text-decoration: none;
+                    text-overflow: ellipsis;
+                    transition: all .14s ease-out;
+                    white-space: nowrap;
+                    :hover {
+                        box-shadow: 4px 4px 0 #000;
+                        transform: translate(-4px,-4px);
+                    }
+                    :focus-visible{
+                        outline-offset: 1px;
+                    }
+                `
+            },
+        ]
     }
 ]
